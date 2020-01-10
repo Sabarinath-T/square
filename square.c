b973b94522ff6c9ad5bd259cda462b38a6c3746b
Sabarinath-T <59721827+Sabarinath-T@users.noreply.github.com>
2020-01-10 12:35:47 +0530

    Update square.c

--- square.c
+++ square.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
+it square(int x);
 int main()
 { int n,s;
 printf("enter the number");
 scanf("%d",&n);
-s=n*n;
+s=square(n);
 printf("square of %d =%d",n,s);
 return 0;
+}
+int square(int x)
+{
+    int z=0;
+    z=x*x;
+    return z;
 }
